96a357574bb5ace96b8c24e4ae05ef9c2c640677
Avery <46545603+ejkeener9@users.noreply.github.com>
2025-04-02 18:30:24 -0700

    Template Topbar Component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import {Component} from '@angular/core';
 import {SidebarComponent} from './sidebar/sidebar.component';
+import {TopbarComponent} from './topbar/topbar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent],
+  imports: [SidebarComponent, TopbarComponent],
   template: `
     <!-- Page Wrapper -->
   <div id="wrapper">
     <app-sidebar></app-sidebar>
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+      <!-- Main Content -->
+      <div id="content">
+        <app-topbar></app-topbar>
+      </div>
+    </div>
   </div>
   `,
   styleUrls: ['./app.component.css'],
