f63af2f53f6e285af9fa0217a624997d0d4bcc48
Avery <46545603+ejkeener9@users.noreply.github.com>
2025-04-02 18:52:35 -0700

    Template Content Component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
 import {SidebarComponent} from './sidebar/sidebar.component';
 import {TopbarComponent} from './topbar/topbar.component';
+import {ContentComponent} from './content/content.component';
 
 @Component({
   selector: 'app-root',
-  imports: [SidebarComponent, TopbarComponent],
+  imports: [SidebarComponent, TopbarComponent, ContentComponent],
   template: `
     <!-- Page Wrapper -->
   <div id="wrapper">
@@ -14,6 +15,7 @@ import {TopbarComponent} from './topbar/topbar.component';
       <!-- Main Content -->
       <div id="content">
         <app-topbar></app-topbar>
+        <app-content></app-content>
       </div>
     </div>
   </div>
